b71f96c5db5f4448a53836b4eb5cd88076f2ecab
daechiDavid <gandy123@daechi.sen.es.kr>
2026-07-20 11:30:34 +0900

    2

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,12 +5,12 @@ import { connectorConfig } from '../lib/dataconnect';
 
 /* global process */
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || 'AIzaSyA6w9fW23z6Se4-efGESIDb8vIfjkVRU-c',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'checkbong-fb3dc.firebaseapp.com',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'checkbong-fb3dc',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || 'checkbong-fb3dc.firebasestorage.app',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '458180956921',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '1:458180956921:web:4ab6f294ec133111c14828'
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
